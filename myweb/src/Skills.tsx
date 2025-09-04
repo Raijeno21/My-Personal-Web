0d5ffe6bc745b7225f4a44a720a0a05415248531
Raijeno21 <jenopogi221@gmail.com>
2025-09-05 06:50:24 +0800

    remove borders in skills

--- myweb/src/Skills.tsx
+++ myweb/src/Skills.tsx
@@ -3,19 +3,19 @@ import { icons } from "./svg";
 const Skills = () => {
   return (
     <div className="h-screen border pt-[13%]">
-      <div className="flex h-full flex-col items-center justify-between border-2 border-blue-500 p-5">
+      <div className="flex h-full flex-col items-center justify-between p-5">
         <div>
           <img src="/images/separator.svg" />
         </div>
-        <div className="flex min-h-0 w-full flex-1 flex-col justify-between border border-red-500 py-5">
-          <div className="flex min-h-0 flex-1 flex-col border border-blue-500">
-            <div className="relative flex border border-green-500">
+        <div className="flex min-h-0 w-full flex-1 flex-col justify-between py-5">
+          <div className="flex min-h-0 flex-1 flex-col">
+            <div className="relative flex">
               <div className="aspect-square h-15">{icons.design}</div>
               <div className="ml-[-25px] flex items-center text-2xl">
                 Design
               </div>
             </div>
-            <div className="flex overflow-y-auto border border-yellow-500">
+            <div className="flex overflow-y-auto">
               <p className="text-justify text-xs">
                 <span className="pl-5"></span> Lorem Ipsum is simply dummy text
                 of the printing and typesetting industry. Lorem Ipsum has been
@@ -30,12 +30,12 @@ const Skills = () => {
               </p>
             </div>
           </div>
-          <div className="flex min-h-0 flex-1 flex-col border border-blue-500">
-            <div className="flex items-center border border-green-500">
+          <div className="flex min-h-0 flex-1 flex-col">
+            <div className="flex items-center">
               <div className="aspect-square h-15">{icons.development}</div>
               <div className="ml-[-25px] text-2xl">Development</div>
             </div>
-            <div className="flex overflow-y-auto border border-yellow-500">
+            <div className="flex overflow-y-auto">
               <p className="text-justify text-xs">
                 <span className="pl-5"></span> Lorem Ipsum is simply dummy text
                 of the printing and typesetting industry. Lorem Ipsum has been
@@ -51,12 +51,12 @@ const Skills = () => {
             </div>
           </div>
 
-          <div className="flex min-h-0 flex-1 flex-col border border-blue-500">
-            <div className="flex items-center border border-green-500">
+          <div className="flex min-h-0 flex-1 flex-col">
+            <div className="flex items-center">
               <div className="aspect-square h-15">{icons.maintenance}</div>
               <div className="ml-[-25px] text-2xl">Maintenance</div>
             </div>
-            <div className="flex overflow-y-auto border border-yellow-500">
+            <div className="flex overflow-y-auto">
               <p className="text-justify text-xs">
                 <span className="pl-5"></span> Lorem Ipsum is simply dummy text
                 of the printing and typesetting industry. Lorem Ipsum has been
